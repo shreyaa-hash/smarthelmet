8d6eccd13e7a1aecbc9bee0ad2e7c048bfe3a8db
Your Name <your.email@example.com>
2026-04-24 01:57:11 +0530

    Add try-catch to login to surface errors

--- backend/api/login.php
+++ backend/api/login.php
@@ -11,25 +11,30 @@ $collection = $db->users;
 
 $data = json_decode(file_get_contents("php://input"));
 
-if(!empty($data->username) && !empty($data->password)) {
-    $user = $collection->findOne(['username' => $data->username]);
+try {
+    if(!empty($data->username) && !empty($data->password)) {
+        $user = $collection->findOne(['username' => $data->username]);
 
-    if($user && password_verify($data->password, $user['password'])) {
-        http_response_code(200);
-        echo json_encode(array(
-            "message" => "Successful login.",
-            "user" => array(
-                "id" => (string)$user['_id'],
-                "username" => $user['username'],
-                "role" => $user['role']
-            )
-        ));
+        if($user && password_verify($data->password, $user['password'])) {
+            http_response_code(200);
+            echo json_encode(array(
+                "message" => "Successful login.",
+                "user" => array(
+                    "id" => (string)$user['_id'],
+                    "username" => $user['username'],
+                    "role" => $user['role']
+                )
+            ));
+        } else {
+            http_response_code(401);
+            echo json_encode(array("message" => "Invalid username or password."));
+        }
     } else {
-        http_response_code(401);
-        echo json_encode(array("message" => "Login failed."));
+        http_response_code(400);
+        echo json_encode(array("message" => "Incomplete data."));
     }
-} else {
-    http_response_code(400);
-    echo json_encode(array("message" => "Incomplete data."));
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(array("message" => "Server Error: " . $e->getMessage()));
 }
 ?>
